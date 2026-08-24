6f986e8a5b2953a69f3cda80d45073b159ee7df9
Alejandro Gutierrez Montufar <alexhhh@hotmail.com>
2026-08-24 16:30:00 -0600

    Update mobile VM address

--- mobile-android/app/src/main/java/mx/edu/accesoindustrial/MaintenanceContainer.kt
+++ mobile-android/app/src/main/java/mx/edu/accesoindustrial/MaintenanceContainer.kt
@@ -5,4 +5,4 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object MaintenanceSession { var token="" }
 // La app entra por Nginx en la VM, no por una réplica del backend directamente.
-object MaintenanceContainer { private val client=OkHttpClient.Builder().addInterceptor(Interceptor{chain->chain.proceed(chain.request().newBuilder().apply{if(MaintenanceSession.token.isNotBlank())header("Authorization","Bearer ${MaintenanceSession.token}")}.build())}).build(); val api:MaintenanceApi by lazy{Retrofit.Builder().baseUrl("http://192.168.1.47/").client(client).addConverterFactory(GsonConverterFactory.create()).build().create(MaintenanceApi::class.java)} }
+object MaintenanceContainer { private val client=OkHttpClient.Builder().addInterceptor(Interceptor{chain->chain.proceed(chain.request().newBuilder().apply{if(MaintenanceSession.token.isNotBlank())header("Authorization","Bearer ${MaintenanceSession.token}")}.build())}).build(); val api:MaintenanceApi by lazy{Retrofit.Builder().baseUrl("http://192.168.1.48/").client(client).addConverterFactory(GsonConverterFactory.create()).build().create(MaintenanceApi::class.java)} }
